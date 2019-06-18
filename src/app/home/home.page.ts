80cd311ae3c6b1a495e47a9b8394c0d63ca9995b
Miss Sai <sai.watcharaphon@gmail.com>
2019-06-18 23:52:07 +0700

    Implement ngx-translate

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NavController, Platform } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(
+    private _platform: Platform,
+    public navCtrl: NavController,
+    private _translate: TranslateService
+  ) {}
 
-  constructor() {}
+  public title: string;
+  public description: string;
+  public language: string;
 
+  public ionViewDidLoad(): void {
+    this._initialiseTranslation();
+  }
+
+  public changeLanguage(): void {
+    this._translateLanguage();
+  }
+
+  private _translateLanguage(): void {
+    this._translate.use(this.language);
+    this._initialiseTranslation();
+  }
+
+  private _initialiseTranslation(): void {
+    setTimeout(() => {
+      this.title = this._translate.instant("home.heading");
+      this.description = this._translate.instant("home.description");
+    }, 250);
+  }
 }
